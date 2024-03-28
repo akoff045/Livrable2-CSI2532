81fa11ff9b39ff8e727ea2f1e0ed2b36e04e1c7a
beast989898 <nightocean@rainbowkiller.slmail.me>
2024-03-28 15:27:09 -0400

    All registration functions

--- website/scripts/client.js
+++ website/scripts/client.js
@@ -1,43 +1,129 @@
+function validateName(name) {
+  var regex = /^(?=.*[a-z])[A-Za-z\s'-]+$/;
+  return regex.test(name) ? "" : "Please enter a valid name";
+}
+
+function validateUsername(username) {
+  var regex = /^[a-zA-Z0-9_-]{1,20}$/;
+  return regex.test(username)
+    ? ""
+    : "Username must be at most 20 characters and contain letters, numbers, dashes, and underscores";
+}
+
+function validatePassword(password) {
+  var regex =
+    /^(?=.*[!@#$%^&*()_\-+=\[\]{};:'",<>.\/?\\|])(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+  return regex.test(password)
+    ? ""
+    : "Password must be at least 8 characters, and contain a lowercase and uppercase letter, a digit, and a symbol";
+}
+
+function validateNum(num) {
+  var regex = /^\d{1,20}$/;
+  return regex.test(num) ? "" : "Please enter a valid number";
+}
+
+function validateZip(zip) {
+  var regex = /^(?=.*[\d])[\d\s]{4,10}$/;
+  return regex.test(zip) ? "" : "Please enter a valid zip code";
+}
+
 document.getElementById("login").addEventListener("click", function (event) {
   event.preventDefault();
-  var name = document.getElementById("name").value;
-  var last = document.getElementById("last").value;
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  var ssn = document.getElementById("ssn").value;
-  var num = document.getElementById("num").value;
-  var street = document.getElementById("street").value;
-  var city = document.getElementById("city").value;
-  var province = document.getElementById("province").value;
-  var country = document.getElementById("country").value;
-  var zip = document.getElementById("zip").value;
 
   var data = {
-    name: name,
-    last: last,
-    username: username,
-    password: password,
-    ssn: ssn,
-    num: num,
-    street: street,
-    city: city,
-    province: province,
-    country: country,
-    zip: zip,
+    name: "",
+    last: "",
+    username: "",
+    password: "",
+    ssn: "",
+    num: "",
+    street: "",
+    city: "",
+    province: "",
+    country: "",
+    zip: "",
   };
 
-  var xhr = new XMLHttpRequest();
-  var url = "/create_client";
+  for (var field in data) {
+    data[field] = document.getElementById(field).value;
+  }
 
-  xhr.open("POST", url, true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      var json = JSON.parse(xhr.responseText);
-      console.log(json);
+  var allFieldsFilled = true;
+  for (var field in data) {
+    if (data[field] === "") {
+      document.getElementById(field).style.borderColor = "red";
+      document.getElementById(field).title = "This field is required";
+      allFieldsFilled = false;
+    } else {
+      document.getElementById(field).style.borderColor = "";
+      document.getElementById(field).title = "";
     }
-  };
+  }
 
-  xhr.send(JSON.stringify(data));
+  if (allFieldsFilled) {
+    var allFieldsValid = true;
+    for (var field in data) {
+      var errorMessage = "";
+      switch (field) {
+        case "name":
+          errorMessage = validateName(data[field]);
+          break;
+        case "last":
+          errorMessage = validateName(data[field]);
+          break;
+        case "username":
+          errorMessage = validateUsername(data[field]);
+          break;
+        case "password":
+          errorMessage = validatePassword(data[field]);
+          break;
+        case "ssn":
+          errorMessage = validateNum(data[field]);
+          break;
+        case "num":
+          errorMessage = validateNum(data[field]);
+          break;
+        case "zip":
+          errorMessage = validateZip(data[field]);
+          break;
+      }
+      if (errorMessage) {
+        document.getElementById(field).style.borderColor = "red";
+        document.getElementById(field).title = errorMessage;
+        allFieldsValid = false;
+      } else {
+        document.getElementById(field).style.borderColor = "";
+        document.getElementById(field).title = "";
+      }
+    }
+  }
+
+  if (allFieldsValid) {
+    var xhr = new XMLHttpRequest();
+    var url = "http://127.0.0.1:3000/create_client";
+
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        var json = JSON.parse(xhr.responseText);
+        console.log(json);
+
+        var usernameInput = document.getElementById("username");
+
+        if (xhr.status === 200) {
+          usernameInput.style.borderColor = "";
+          usernameInput.title = "";
+          window.location.href = "login.html";
+        } else if (xhr.status === 400) {
+          usernameInput.style.borderColor = "red";
+          usernameInput.title = json.message;
+        }
+      }
+    };
+
+    xhr.send(JSON.stringify(data));
+  }
 });
--- website/server.js
+++ website/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(cors());
@@ -20,17 +20,64 @@ app.post("/create_client", (req, res) => {
   const client = req.body;
 
   pool.query(
-    "INSERT INTO personne (nas, nom, prenom) VALUES ($1, $2, $3)",
-    [client.ssn, client.last, client.name],
+    "SELECT * FROM client WHERE username = $1",
+    [client.username],
     (error, results) => {
       if (error) {
         throw error;
       }
-      res.status(200).json({ status: "success", message: "Client added." });
+
+      if (results.rows.length > 0) {
+        res
+          .status(400)
+          .json({ status: "error", message: "Username already exists." });
+      } else {
+        pool.query(
+          "INSERT INTO address (num, street, city, province, country, code_post) VALUES ($1, $2, $3, $4, $5, $6) RETURNING address_id",
+          [
+            client.num,
+            client.street,
+            client.city,
+            client.province,
+            client.country,
+            client.zip,
+          ],
+          (error, results) => {
+            if (error) {
+              throw error;
+            }
+
+            const addressId = results.rows[0].address_id;
+
+            pool.query(
+              "INSERT INTO personne (nas, prenom, nom, address_id) VALUES ($1, $2, $3, $4)",
+              [client.ssn, client.name, client.last, addressId],
+              (error, results) => {
+                if (error) {
+                  throw error;
+                }
+
+                pool.query(
+                  "INSERT INTO client (nas, username, password, date_enrg) VALUES ($1, $2, $3, CURRENT_DATE)",
+                  [client.ssn, client.username, client.password],
+                  (error, results) => {
+                    if (error) {
+                      throw error;
+                    }
+                    res
+                      .status(200)
+                      .json({ status: "success", message: "Client added." });
+                  }
+                );
+              }
+            );
+          }
+        );
+      }
     }
   );
 });
 
-app.listen(5500, () => {
-  console.log("Server is running on port 5500");
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
